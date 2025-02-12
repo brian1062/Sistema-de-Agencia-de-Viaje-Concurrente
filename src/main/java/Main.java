6f5b8fa36a574e490d559b0ae8b4e366b3da00f0
Franco Viotti <fviotti@unc.edu.ar>
2025-02-12 00:46:07 -0300

    delete old Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -60,36 +60,3 @@ public class Main {
     }
   }
 }
-
-/*public class Main {
-
-  public static void main(String[] args) {
-
-    PetriNetConf rdPConf = new PetriNetConf(); // Load configuration
-
-    PetriNet petriNet =
-        new PetriNet(
-            rdPConf.getTransitions(),
-            rdPConf.getPlaces(),
-            rdPConf.getIncidenceMatrixOut(),
-            rdPConf.getIncidenceMatrixIn(),
-            rdPConf.getPlacesInvariants(),
-            rdPConf.getInitialMarking(),
-            rdPConf.getTargetInvariants());
-
-    // Initialize threads array
-    Thread[] threads =
-        new Thread
-            [rdPConf.getNumberOfSequences()]; // We'll have one thread per sequence of transitions
-
-    // Create the desired policy
-    Policy policy = new PrioritizedPolicy();  // or new BalancedPolicy()
-
-    Monitor monitor = Monitor.getMonitor(petriNet, policy);
-
-    // Create and start threads
-    Arrays.setAll(
-        threads, i -> new Thread(new Segments(rdPConf.getTransitionSequence(i), monitor)));
-    Arrays.stream(threads).forEach(Thread::start);
-  }
-}*/
