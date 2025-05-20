9d9a7d29a5c1a2b97d12344b819eb82b650dc08a
Franco Viotti <fviotti@unc.edu.ar>
2025-05-20 01:33:19 -0300

    Segments receive the petriNet in their constructor

--- src/main/java/petrinet/Segments.java
+++ src/main/java/petrinet/Segments.java
@@ -10,16 +10,19 @@ import monitor.Monitor;
 public class Segments implements Runnable {
   private final List<Transition> sequence;
   private final Monitor monitor;
+  private final PetriNet petriNet;
 
   /**
    * Constructor for the Segments instance with a trnsition sequence.
    *
    * @param sequence The sequence of transitions to execute.
    * @param monitor The monitor instance to control the Petri Net execution.
+   * @param petriNet The Petri Net instance to be controlled.
    */
-  public Segments(List<Transition> sequence, Monitor monitor) {
+  public Segments(List<Transition> sequence, Monitor monitor, PetriNet petriNet) {
     this.sequence = sequence;
     this.monitor = monitor;
+    this.petriNet = petriNet;
   }
 
   /**
@@ -37,7 +40,7 @@ public class Segments implements Runnable {
         monitor.fireTransition(t.getNumber());
 
         // Check if the Petri Net has finished after firing the transition
-        if (monitor.petriNetHasFinished()) {
+        if (petriNet.petriNetHasFinished()) {
           System.out.println("Thread " + Thread.currentThread().getName() + " has finished.");
           finished = true;
           break;
